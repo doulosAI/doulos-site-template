0413133ef9c319aa4ebcbbb0bf972f5eb7615776
doulosAI <soldnetic@gmail.com>
2026-06-27 08:46:22 -0600

    init

---
+++ components/index.ts
@@ -0,0 +1,14 @@
+// Layout
+export { default as LenisWrapper } from "./layout/LenisWrapper"
+export { default as PageTransition } from "./layout/PageTransition"
+export { default as BottomNav } from "./layout/BottomNav"
+
+// Animations
+export { StaggerContainer, StaggerItem } from "./animations/StaggerReveal"
+export { default as SectionReveal } from "./animations/SectionReveal"
+export { default as AmbientBackground } from "./animations/AmbientBackground"
+export { default as TextReveal } from "./animations/TextReveal"
+export { default as SwipeableCards } from "./animations/SwipeableCards"
+
+// Chatbot
+export { default as ChatbotWidget } from "./chatbot/ChatbotWidget"
